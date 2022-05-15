a51d36a1efff65336a6abe1e60b9ea6c85ba8fa0
Estevao25 <estevaolouro2002@gmail.com>
2022-05-15 03:33:14 -0300

    Some additional tweaks

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -8,11 +8,22 @@ type Props = {
 
 function MovieScore({score, count}:Props) {
 
+    function renderScoreCount(){
+        if(count===0){
+            return <p className="dsmovie-score-count">Nenhuma avaliação</p>;
+        }else if(count===1){
+            return <p className="dsmovie-score-count">{count} avaliação</p>;
+        }else{
+            return <p className="dsmovie-score-count">{count} avaliações</p>;
+        }
+
+     }
+
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
             <MovieStars score={score} />
-            <p className="dsmovie-score-count">{count} avaliações</p>
+            { renderScoreCount() }
         </div>
     );
 }
